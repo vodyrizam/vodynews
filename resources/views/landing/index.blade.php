16d2f37d684110532d1ec354e33f24dbf7e667bf
vodyrizam <vodyrizam@vodyrizams-MacBook-Air.local>
2025-12-16 13:01:32 +0700

    ‘update’

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,9 +1,71 @@
 @extends('layouts.app')
 @section('title','Beranda')
 @section('content')
-<h1 class="mb-3">Portal Berita</h1>
-<p>Total Berita Dipublikasikan: <strong>{{ $total }}</strong></p>
+<h1 class="mb-3">Ternyata test berita</h1>
+
 <div class="row">
+        <!-- ##### Hero Area Start ##### -->
+    <div class="hero-area">
+        <!-- Hero Post Slides -->
+        <div class="hero-post-slides owl-carousel">
+
+            <!-- Single Slide -->
+            <div class="single-slide">
+                <div class="container-fluid">
+                    <div class="row">
+                        <!-- Single Blog Post Area -->
+                        <div class="col-12 col-md-6">
+                            <div class="single-blog-post style-1" data-animation="fadeInUpBig" data-delay="100ms" data-duration="1000ms">
+                                <!-- Blog Thumbnail -->
+                                <div class="blog-thumbnail bg-overlay">
+                                    <a href="#"><img src="{{ ('assets/img/bg-img/1.jpg') }}"></a>
+                                </div>
+
+                                <!-- Blog Content -->
+                                <div class="blog-content">
+                                    <span class="post-date">June 20, 2018</span>
+                                    <a href="#" class="post-title">Traffic Problems in Time Square</a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-12 col-md-6">
+                            <!-- Single Blog Post Area -->
+                            <div class="single-blog-post style-1 mb-30" data-animation="fadeInUpBig" data-delay="300ms" data-duration="1000ms">
+                                <!-- Blog Thumbnail -->
+                                <div class="blog-thumbnail bg-overlay">
+                                    <a href="#"><img src="{{ ('assets/img/bg-img/2.jpg') }}"></a>
+                                </div>
+
+                                <!-- Blog Content -->
+                                <div class="blog-content">
+                                    <span class="post-date">June 20, 2018</span>
+                                    <a href="#" class="post-title">The best way to spend your holliday</a>
+                                </div>
+                            </div>
+                            <!-- Single Blog Post Area -->
+                            <div class="single-blog-post style-1" data-animation="fadeInUpBig" data-delay="500ms" data-duration="1000ms">
+                                <!-- Blog Thumbnail -->
+                                <div class="blog-thumbnail bg-overlay">
+                                    <a href="#"><img src="{{ ('assets/img/bg-img/3.jpg') }}"></a>
+                                </div>
+
+                                <!-- Blog Content -->
+                                <div class="blog-content">
+                                    <span class="post-date">June 20, 2018</span>
+                                    <a href="#" class="post-title">Sport results for the weekend games</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <!-- ##### Hero Area End ##### -->
+    <p>Total Berita Dipublikasikan: <strong>{{ $total }}</strong></p>
 @foreach($latest as $item)
 <div class="col-md-4 mb-3">
 <div class="card h-100">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 <meta name="description" content="Portal Berita Laravel">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/
 bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ ('assets/css/style.css') }}">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
